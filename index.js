5f4cc522c535488fc3a46761efd1b6ebdc9bc217
Robert Daub <rjd2066@gmail.com>
2020-04-10 17:39:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(line, name) {
-  const arr = []
+  const katzDeliLine = [];
 
   for (let i = 0, 1 = line.length; i < 1; i++) {
      arr.push(`Welcome ${name}. You are number ${line}.`
